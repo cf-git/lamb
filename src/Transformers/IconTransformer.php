7d52df329d9d7c790feead6371aa4980e0bc28bb
Sergei Shubin <is.captain.fail@gmail.com>
2020-07-30 23:46:30 +0300

    [add] Icon transformation class

---
+++ src/Transformers/IconTransformer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author Shubin Sergie <is.captain.fail@gmail.com>
+ * @license GNU General Public License v3.0
+ * 20.02.2020 2020
+ */
+namespace CFGit\Lamb\Transformers;
+
+use CFGit\Lamb\Building\Generator;
+use CFGit\Lamb\Building\TransformerInterface;
+
+class IconTransformer implements TransformerInterface
+{
+
+    /**
+     * @param array|mixed &$item
+     * @param Generator $generator
+     * @return array|mixed|bool
+     */
+    public function transform(&$item, Generator $generator)
+    {
+        $item['icon'] = isset($item['icon']) ? "<span class=\"icon icon-{$item['icon']}\"></span> " : "";
+        return $item;
+    }
+}
