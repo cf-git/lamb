16238bd453b5e3409ca48f60eabfc570fdaadef4
cf-git <is.captain.fail@gmail.co,m>
2021-04-07 00:33:30 +0300

    [rebuild] part 3

--- src/resources/config/lamb/main.php
+++ src/resources/config/lamb/main.php
@@ -6,20 +6,7 @@
  * 05.02.2020 2020
  */
 
-use CFGit\Lamb\Transformations\LinkTransformation;
-use CFGit\Lamb\Transformations\SpanIconTransformation;
-use CFGit\Lamb\Transformations\SubmenuTransformation;
-use CFGit\Lamb\Transformations\MenuTranslateTransformation;
-use CFGit\Lamb\Transformations\FontawesomeIconTransformation;
-
 return [
-    'transformations' => [
-        LinkTransformation::class,
-        SubmenuTransformation::class,
-        SpanIconTransformation::class,
-        MenuTranslateTransformation::class,
-        FontawesomeIconTransformation::class,
-    ],
     'menu' => [
     ],
 ];
